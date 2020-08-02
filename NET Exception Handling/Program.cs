762ce29eb843ac24f2b94832abfa43a51902c98d
mohan-kukreja <mohan.kukreja@accoliteindia.com>
2020-08-03 00:20:01 +0530

    net assignment added

---
+++ NET Exception Handling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net_Practice
+{
+    
+    public class demo{
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"/Users/mohan/Desktop/sample.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found!" + ex);
+               
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Directory not found" + ex);
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception caught" + ex);
+              
+            }
+            finally
+            {
+                Console.WriteLine("Closing application" );
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+    
+}
---
+++ NET Generics/Generic Stack/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net_Practice
+{
+   public class footballer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+    }
+    
+    public class demo{
+        static void Main(string[] args)
+        {
+            footballer fb1 = new footballer(){
+                ID= 7,
+                Name= "Cristiano Ronaldo",
+                Position= "LW",
+                Age= 35
+            };
+            footballer fb2 = new footballer(){
+                ID= 10,
+                Name= "Lionel Messi",
+                Position= "CAM",
+                Age= 33
+            };
+            footballer fb3 = new footballer(){
+                ID= 4,
+                Name= "Sergio Ramos",
+                Position= "CB",
+                Age= 35
+            };
+            footballer fb4 = new footballer(){
+                ID= 10,
+                Name= "Eiden Hazard",
+                Position= "ST",
+                Age= 27
+            };
+            Stack<footballer> q = new Stack<footballer>();
+            q.Push(fb1);
+            q.Push(fb2);
+            q.Push(fb3);
+            q.Push(fb4);
+
+            while(q.Any()){
+                footballer temp = q.Peek();
+                
+                Console.WriteLine(temp.Name);
+                q.Pop();
+            }
+        }
+
+        
+    }
+    
+}
